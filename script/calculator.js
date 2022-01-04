bee56a04ade8540aaa2df84e1a68f24f03d08b28
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-04 22:06:30 +0800

    calculator day1

--- script/calculator.js
+++ script/calculator.js
@@ -1,7 +1,7 @@
 const displayElement1 = document.querySelector('.cal-display1');
 const displayElement2 = document.querySelector('.cal-display2');
 const tempResultElement = document.querySelector('.cal-temp-result');
-const numberElement = document.querySelector('.cal-number');
+const numbersElement = document.querySelector('.cal-number');
 const operationElement = document.querySelector('.cal-operation');
 const allClearElement = document.querySelector('.cal-all-clear');
 const lastEntryClearElement = document.querySelector('.cal-last-entry-clear');
@@ -10,9 +10,19 @@ const dotElement = document.querySelector('.cal-dot');
 
 let miniDisplay = '';
 let mainDisplay = '';
-let tempResult = '';
+let tempResult = null;
 let lastOperation = '';
 let haveDot = false;
 
-
+numbersElement.forEach( number => {
+    number.addEventListener('click', (e) => {
+        if( e.target.innerText === '.' && !haveDot ){
+            haveDot = true;
+        } else if(e.target.innerText === '.' && haveDot){
+            return;
+        }
+        mainDisplay += e.target.innerText;
+        displayElement2.innerText = mainDisplay;
+    })
+})
 
