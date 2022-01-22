60841522bd6701dce6055e7ab9e111942db22589
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-22 21:42:43 +0800

    onclick effect

--- script/script.js
+++ script/script.js
@@ -6,7 +6,22 @@ menuToggle.addEventListener('click', () => {
   showcase.classList.toggle('active')
 })
 
+const buttons = document.querySelectorAll('.ripple')
 
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const xInside = e.clientX
+        const yInside = e.clientY
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+        setTimeout(() => circle.remove(), 500)
+    })
+})
 
 
 
