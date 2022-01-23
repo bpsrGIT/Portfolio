53d90ef2dc5b9f3ef4db4bfd6ca61eb45e0ae4a0
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-23 15:40:24 +0800

    contact

--- script/script.js
+++ script/script.js
@@ -1,16 +1,13 @@
-const Swal = require('sweetalert2')
-const menuToggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.mainBody')
-const contactBtn = document.querySelector('.button-contact')
 
-
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.mainBody');
+const contactBtn = document.querySelector('.button-contact');
+const buttons = document.querySelectorAll('.ripple')
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
   showcase.classList.toggle('active')
 })
-
-const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
@@ -27,25 +24,25 @@ buttons.forEach(button => {
     })
 })
 
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-})
-const handleSubmit = () => {
-  Toast.fire({
-    icon: 'success',
-    title: 'Inquiry Sent',
-    text: 'will respond to you asap'
-  })
-}
 contactBtn.addEventListener('click', ()=>{
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  const handleSubmit = () => {
+    Toast.fire({
+      icon: 'success',
+      title: 'Inquiry Sent',
+      text: 'will respond to you asap'
+    })
+  }
   handleSubmit()
 })
 
