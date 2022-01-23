2d3beb9adce628c34ecf3efb0e4f034f26986a31
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-23 16:38:09 +0800

    contact form working

--- script/script.js
+++ script/script.js
@@ -46,5 +46,24 @@ contactBtn.addEventListener('click', ()=>{
   handleSubmit()
 })
 
-
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Inquiry submitted! Will respond as soon as possible!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
