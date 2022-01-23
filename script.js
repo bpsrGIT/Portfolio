75c95d1e94a33a366da10b8b0032b744fc39dd49
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-23 18:05:58 +0800

    housekeeping

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.mainBody');
-const contactBtn = document.querySelector('.button-contact');
 const buttons = document.querySelectorAll('.ripple')
+
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
   showcase.classList.toggle('active')
 })
 
+// Ripple on mouse click
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
         const xInside = e.clientX
@@ -24,28 +25,7 @@ buttons.forEach(button => {
     })
 })
 
-contactBtn.addEventListener('click', ()=>{
-  const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 2000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
-  const handleSubmit = () => {
-    Toast.fire({
-      icon: 'success',
-      title: 'Inquiry Sent',
-      text: 'will respond to you asap'
-    })
-  }
-  handleSubmit()
-})
-
+// Submission form
 var form = document.getElementById("my-form");
     
 async function handleSubmit(event) {
@@ -66,4 +46,3 @@ async function handleSubmit(event) {
   });
 }
 form.addEventListener("submit", handleSubmit)
-
